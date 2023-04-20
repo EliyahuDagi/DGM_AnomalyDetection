1d8ea192c0a05d33fc036df7d4541a37fc81221a
RND <rnd@Au10tix.com>
2023-04-21 01:53:57 +0300

    VAE comments

--- models/VAE.py
+++ models/VAE.py
@@ -33,11 +33,12 @@ def base_nn(in_channels, feat_channels, out_channels):
 
 class VAE(nn.Module, GenerativeModel):
     def __init__(self, in_channels, latent_dim, input_shape, type):
-        """Initialize a VAE.
+        """
 
-        Args:
-            in_channels: number of input channels 1 - grayscale 3 - color or arbitrary size
-            latent_dim: dimension of embedding
+        :param in_channels: number of input channels 1 - grayscale 3 - color or arbitrary size
+        :param latent_dim: dimension of mu and log var
+        :param input_shape: the shape of the input
+        :param type: original/features
         """
         nn.Module.__init__(self)
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -45,6 +46,7 @@ class VAE(nn.Module, GenerativeModel):
         self.feat_channels = 16
         self.img_size = input_shape[-1]
         self.in_norm = nn.Identity() if type == 'original' else nn.Sigmoid()
+        # sequence of residual block + max pooling
         self.encoder = nn.Sequential(ResNet(in_channels, 32, 32),
                                      nn.MaxPool2d(2),  # B,  32, 16, 16
                                      ResNet(32, 32, 32),
@@ -85,10 +87,12 @@ class VAE(nn.Module, GenerativeModel):
         return self.decode(z_sample)
 
     def z_sample(self, mu, log_var):
+        # re parametrization trick
         eps = torch.randn_like(mu).to(self.device)
         return mu + torch.exp(log_var * 0.5) * eps
 
     def ELBO(self, x, reconstruction, mu, log_var):
+        # calculate reconstruction and kl divergence term
         reconstruct_term = torch.sum(self.bce(reconstruction, self.in_norm(x)), dim=[1, 2, 3])
         kl_term = torch.sum(-0.5 * (1 + log_var - (mu * mu) - torch.exp(log_var)), dim=1)
         return -reconstruct_term - kl_term
@@ -118,6 +122,7 @@ class VAE(nn.Module, GenerativeModel):
         encoded = nn.Flatten()(self.encoder(self.in_norm(x)))
         mu = self.mu(encoded)
         log_var = self.logvar(encoded)
+        # sample from q(z|x)
         z_sample = self.z_sample(mu=mu, log_var=log_var)
         reconstruction = self.decode(z_sample)
         return {'x': x,
@@ -125,5 +130,3 @@ class VAE(nn.Module, GenerativeModel):
                 'log_var': log_var,
                 'reconstruction': reconstruction
                 }
-
-
